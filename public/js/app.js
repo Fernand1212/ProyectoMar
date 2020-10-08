19632af942bfe70f16fb70d3943320158bd29205
Fernand1212 <fernann90@gmail.com>
2020-10-08 06:49:16 -0300

    primer commit posta posta

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /media/fernann90/particionHDD/Proyecto Mar/resources/js/app.jsx */"./resources/js/app.jsx");
-module.exports = __webpack_require__(/*! /media/fernann90/particionHDD/Proyecto Mar/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/finix/Proyecto Mar/resources/js/app.jsx */"./resources/js/app.jsx");
+module.exports = __webpack_require__(/*! /home/finix/Proyecto Mar/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
