21e5c40913522ff0a381c94195288aedb2ea82c0
Fernand1212 <fernann90@gmail.com>
2020-10-07 21:02:02 -0300

    prueba3mil

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+      @include('includes.head')
+    </head>
+    <a><i class="fab fa-whatsapp" id="aa" >  </i></a>
+        @include('includes.header')
+    <body class="color">
+      <div class="body">
+        <article class="">
+
+        </article>
+
+      </div>
+
+
+
+
+
+
+    </body>
+    <div class="">
+      @include('includes.footer')
+    </div>
+
+</html>
